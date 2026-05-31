dfe8c88fd06842590b7dc2ddc3f29dfdd9d3a9c2
Md-Galib5 <mdgalibcp05@gmail.com>
2026-05-31 15:25:48 +0600

    post some data and install postman

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel,HttpUrl
 
 app = FastAPI()
+
+class travel(BaseModel):
+    country: str
+    city: str
+    duration: str
+    cost: str
+    website: HttpUrl
+
+@app.post("/germany")
+def create_post(post : travel):
+    return {"data" : post}
+
+@app.get("/germany")
+def get_travels():
+    return travel
 
 @app.get("/")
 def get_api():
     return {"django" : "fastapi"}
+
+@app.get("/hungary")
+def hungary():
+    return {"budapest" : "Pecs"}
